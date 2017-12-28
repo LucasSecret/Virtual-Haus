25c949e35bb9741354e2ea701e2cf153e69dc943
LucasSecret <lucas.secret@orange.fr>
2017-12-28 19:37:20 +0100

    [Fix] Fix commit issues

---
+++ UnityProject/PTUT_VR_MOZARTHAUS/Assets/Scripts/NonVR/LaserHandlerNonVR.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserHandlerNonVR : MonoBehaviour {
+    Mod mod;
+    LineRenderer laserLine;
+
+    // Use this for initialization
+    void Start () {
+        mod = ModHandlerNonVR.mod;
+        laserLine = this.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        UpdateMod();
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+            UpdateLaserPos(hit);
+    }
+
+
+    void UpdateLaserPos(RaycastHit hit)
+    {
+        laserLine.SetPosition(1, hit.point);
+        laserLine.SetPosition(0, new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z - 0.1f));
+
+        UpdateLaserColor();
+    }
+    void UpdateLaserColor()
+    {
+        if (mod == Mod.UTILITIES)
+            laserLine.GetComponent<Renderer>().material.color = Color.blue;
+        else
+            laserLine.GetComponent<Renderer>().material.color = Color.green;
+
+    }
+    void UpdateMod()
+    {
+        mod = ModHandlerNonVR.mod;
+    }
+}
---
+++ UnityProject/PTUT_VR_MOZARTHAUS/Assets/Scripts/NonVR/TeleportationNonVR.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationNonVR : MonoBehaviour {
+
+    public GameObject player;
+
+    private bool canTeleport = true;
+
+    private Mod mod;
+
+    
+
+    /******************/
+    /* Initialisation */
+    /******************/
+
+    void Start() {
+       
+    }
+
+
+    /**********/
+    /* Update */
+    /**********/
+
+    void Update()
+    {
+        
+      
+        UpdateMod();
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (IsTriggerClicked() && canTeleport && mod == Mod.UTILITIES)
+        {
+            if (Physics.Raycast(ray, out hit, 10000.0f))
+            {
+                Teleport(player, hit);
+                canTeleport = false;
+            }
+        }
+        else if (!IsTriggerClicked())
+        {
+            canTeleport = true;
+        }
+        
+    }
+
+
+
+
+    void UpdateMod()
+    {
+        this.mod = ModHandlerNonVR.mod;
+    }
+
+    bool IsTriggerClicked()
+    {
+        return (Input.GetMouseButtonDown(0));
+    }
+
+
+    void Teleport(GameObject gameObject, RaycastHit destination)
+    {
+        Vector3 newPos = destination.point;
+        newPos.y = 0;
+
+        gameObject.transform.position = destination.point;
+    }
+
+}
+
+
+
