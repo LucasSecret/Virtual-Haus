0541dd9b2dfd19be52ef85b68f7f9cf7dcaa9d67
Braun <tenzin.braun@gmail.com>
2017-12-31 00:41:19 +0100

    [UI + code + IHM] change IHM with correct TP + ligth clean code

--- UnityProject/PTUT_VR_MOZARTHAUS/Assets/Scripts/NonVR/HomeUIHandler.cs
+++ UnityProject/PTUT_VR_MOZARTHAUS/Assets/Scripts/NonVR/HomeUIHandler.cs
@@ -5,48 +5,42 @@ using UnityEngine.UI;
 
 public class HomeUIHandler : MonoBehaviour
 {
-    private Mod mod;
-
     private const string CLICKED_TRIGGER_NAME = "PointerTrigger";
-    private const string CLICKED_BUTTON_TRIGGER_NAME = "PointerTrigger";
 
     private bool canClick = true;
 
     public GameObject player;
-
-    Vector3 newCameraPos;
-
-
-    RayCast rayCast;
+    private Vector3 mozartHausSpawn;
+    private RayCast rayCast;
     
 
     public void Start()
     {
         rayCast = GameObject.Find("PointerController").GetComponent<RayCast>();
     }
-    public void Update()
-    {
 
-        UpdateMod();
-      
+    public void Update()
+    {     
 
         if (IsTriggerClicked() && canClick)
-        { 
+        {
+            if (rayCast.GetHit().transform.name != null)
+            {
 
-            if (rayCast.GetHit().transform.name.ToString() == "MozartHaus")
-            {
-                TeleportToStartPosition(player);
-                canClick = false;
+                if (rayCast.GetHit().transform.name.ToString() == "MozartHaus")
+                {
+                    TeleportToStartPosition(player);
+                    canClick = false;
 
-                mod = Mod.UTILITIES;        
-            }
-            else if(rayCast.GetHit().transform.name.ToString() == "Appartment")
-            {
-                print("Appartment");
-            }
-            else if(rayCast.GetHit().transform.name.ToString() == "SettingsImport")
-            {
-                print("SettingsImport");
+                }
+                else if (rayCast.GetHit().transform.name.ToString() == "Appartment")
+                {
+                    print("Appartment");
+                }
+                else if (rayCast.GetHit().transform.name.ToString() == "SettingsImport")
+                {
+                    print("SettingsImport");
+                }
             }
 
         }
@@ -56,21 +50,16 @@ public class HomeUIHandler : MonoBehaviour
         }
     }
 
-    bool IsTriggerClicked()
+    private bool IsTriggerClicked()
     {
-        return (Input.GetAxis(CLICKED_TRIGGER_NAME) == 1 || Input.GetButton(CLICKED_BUTTON_TRIGGER_NAME));
-    }
-    void UpdateMod()
-    {
-        this.mod = ModHandlerNonVR.mod;
+        return (Input.GetAxis(CLICKED_TRIGGER_NAME) == 1 || Input.GetButton(CLICKED_TRIGGER_NAME));
     }
 
-    void TeleportToStartPosition(GameObject gameObject)
+    private void TeleportToStartPosition(GameObject gameObject)
     {
-        newCameraPos.y = (float) 0.574;
-        newCameraPos.x = -2;
-        newCameraPos.z = (float)-3.555;
-        gameObject.transform.position = newCameraPos;
+        mozartHausSpawn.Set((float)-2, (float)0.574, (float)-3.555);
+        gameObject.transform.position = mozartHausSpawn;
+       
     }
 
 }
