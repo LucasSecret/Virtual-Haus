bdab0c4b0d33242bb6023b1cf003cbc01877d3a4
Balthov60 <Balthazarfrolin@gmail.com>
2017-12-31 16:57:30 +0100

    [Input Manager] first version of IM

--- UnityProject/PTUT_VR_MOZARTHAUS/Assets/Scripts/ModHandler.cs
+++ UnityProject/PTUT_VR_MOZARTHAUS/Assets/Scripts/ModHandler.cs
@@ -6,38 +6,24 @@ public class ModHandler : MonoBehaviour {
 
 
     public static Mod mod = Mod.UTILITIES;
-    private static string MENU_CHANGE_BUTTON_NAME = "ChangeMenuButton";
-
-    private bool canChange = true;
+    private InputManager inputManager;
 
 	void Start () {
-		
+        inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
         Debug.Log(mod);
 
-        if (IsModChangeButtonClicked() && canChange)
+        if (inputManager.IsSwitchMenuButtonClicked())
         {
+            Debug.Log("Change Mod");
             if (mod == Mod.UTILITIES)
                 mod = Mod.EDITION;
             else
                 mod = Mod.UTILITIES;
-                 
-            canChange = false;
         }
-        else if (!IsModChangeButtonClicked())
-        {
-            canChange = true;
-        }
-
-    }
-
-    private bool IsModChangeButtonClicked()
-    {
-        return Input.GetButton(MENU_CHANGE_BUTTON_NAME);
     }
 }
 
