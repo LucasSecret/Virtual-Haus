ce43c1c576f1b52ba846f92094ef4f0c5fdd074d
Balthov60 <Balthazarfrolin@gmail.com>
2018-02-18 18:51:26 +0100

    reimplement letter selector

---
+++ UnityProject/Assets/Scripts/UIHandlers/SavingUIHandlers/IdSelectorUI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdSelectorUI : MonoBehaviour {
+
+    private RayCast rayCast;
+    private InputManager inputManager;
+
+    private bool canClick;
+
+    void Start ()
+    {
+        rayCast = GameObject.Find("PointerController").GetComponent<RayCast>();
+        inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
+
+        canClick = true;
+    }
+
+    void Update()
+    {
+        if (inputManager.IsTriggerClicked() && rayCast.Hit() && canClick)
+        {
+            canClick = false;
+
+            if (rayCast.GetHit().transform.name == "ButtonDown")
+            {
+                Text currentLetterSelector = rayCast.GetHit().transform.parent.Find("LetterView").GetComponentInChildren<Text>();
+                currentLetterSelector.text = GetNextChar(currentLetterSelector.text[0]).ToString();
+            }
+            else if (rayCast.GetHit().transform.name == "ButtonUp")
+            {
+                Text currentLetterSelector = rayCast.GetHit().transform.parent.Find("LetterView").GetComponentInChildren<Text>();
+                currentLetterSelector.text = GetPreviousChar(currentLetterSelector.text[0]).ToString();
+            }
+        }
+
+        if (!canClick)
+        {
+            canClick = !inputManager.IsTriggerClicked();
+        }
+    }
+
+    private char GetNextChar(char c)
+    {
+        int ascii = c;
+
+        ascii = (++c - 65) % 26 + 65;
+
+        return (char)ascii;
+    }
+    private char GetPreviousChar(char c)
+    {
+        // Forced to use a test negative modulo doesn't work
+        int ascii = c;
+        ascii = (--c - 65);
+
+        if (ascii < 0)
+        {
+            ascii += 26;
+        }
+        ascii = ascii % 26 + 65;
+
+        return (char)ascii;
+    }
+}
