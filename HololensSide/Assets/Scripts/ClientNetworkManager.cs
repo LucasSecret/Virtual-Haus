6da6bd17642110410206dcdbd6ff6b8b51bd106a
Balthov60 <Balthazarfrolin@gmail.com>
2018-02-19 11:43:55 +0100

    First Imp of RA Socket

---
+++ HololensSide/Assets/Scripts/ClientNetworkManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClientNetworkManager : MonoBehaviour {
+
+    private static readonly int NETWORK_PORT = 4875;
+    private static readonly string NETWORK_IP_ADDRESS = "127.0.0.1";
+
+    private NetworkClient client;
+    private ClientStatus status;
+
+    private Transform player;
+
+    void Start()
+    {
+        status = ClientStatus.DISCONNECTED;
+
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if (status == ClientStatus.DISCONNECTED)
+            SetupClient();
+    }
+
+    public void SetupClient()
+    {
+        client = new NetworkClient();
+        client.RegisterHandler(MsgType.Connect, OnConnected);
+        client.RegisterHandler(VirtualHausMessageTypes.APPARTMENT_LOADING, LoadAppartment);
+        client.RegisterHandler(VirtualHausMessageTypes.FURNITURE_LOADING, LoadFurnitures);
+
+        client.RegisterHandler(VirtualHausMessageTypes.USER_POSITION, UpdateUserPosition);
+
+        client.Connect(NETWORK_IP_ADDRESS, NETWORK_PORT);
+        status = ClientStatus.CONNECTING;
+    }
+
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        client.Send(VirtualHausMessageTypes.CONNECTED, new EmptyMessage());
+        status = ClientStatus.CONNECTED;
+    }
+    public void LoadAppartment(NetworkMessage netMsg)
+    {
+        AppartmentLoadingMessage msg = netMsg.ReadMessage<AppartmentLoadingMessage>();
+
+        GameObject appartment = Instantiate(Resources.Load("Appartments/MozartHaus/MozartHaus")) as GameObject;
+        appartment.transform.position = msg.appartmentPosition;
+        appartment.transform.localScale = msg.appartmentScale;
+
+        client.Send(VirtualHausMessageTypes.APPARTMENT_LOADED, new EmptyMessage());
+        status = ClientStatus.APPARTMENT_LOADED;
+    }
+
+    public void LoadFurnitures(NetworkMessage netMsg)
+    {
+        Debug.Log("receive furnitures");
+        FurnitureLoadingMessage msg = netMsg.ReadMessage<FurnitureLoadingMessage>();
+
+        PlaceFurniture(JsonUtility.FromJson<List<SavedGameObject>>(msg.jsonFurnitures));
+
+        client.Send(VirtualHausMessageTypes.USER_READY, new EmptyMessage());
+        status = ClientStatus.READY;
+    }
+    private void PlaceFurniture(List<SavedGameObject> furnitures)
+    {
+        //TODO: Implement it
+    }
+
+    private void UpdateUserPosition(NetworkMessage netMsg)
+    {
+        UserPositionMessage msg = netMsg.ReadMessage<UserPositionMessage>();
+
+        player.position = msg.userPosition;
+        player.rotation = msg.userRotation;
+    }
+
+
+    public enum ClientStatus
+    {
+        DISCONNECTED,
+        CONNECTING,
+        CONNECTED,
+        APPARTMENT_LOADED,
+        READY
+    }
+}
---
+++ UnityProject/Assets/Scripts/ServerNetworkManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerNetworkManager : MonoBehaviour
+{
+    public GameObject currentAppartment;
+    private Transform player;
+
+    private static readonly int NETWORK_PORT = 4875;
+    private Dictionary<int, bool> connectionsReady;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+
+        connectionsReady = new Dictionary<int, bool>();
+        SetupServer();
+    }
+
+    void Update()
+    {
+        UserPositionMessage userPositionMessage = new UserPositionMessage()
+        {
+            userPosition = player.position,
+            userRotation = player.rotation
+        };
+
+        foreach (KeyValuePair<int, bool> connection in connectionsReady)
+        {
+            if (connection.Value == true)
+            {
+                NetworkServer.SendToClient(connection.Key, VirtualHausMessageTypes.USER_POSITION, userPositionMessage);
+            }
+        }
+    }
+
+    public void SetupServer()
+    {
+        NetworkServer.RegisterHandler(VirtualHausMessageTypes.CONNECTED, OnClientConnect);
+        NetworkServer.RegisterHandler(VirtualHausMessageTypes.APPARTMENT_LOADED, OnClientAppartmentLoaded);
+        NetworkServer.RegisterHandler(VirtualHausMessageTypes.USER_READY, OnClientReady);
+
+        NetworkServer.Listen(NETWORK_PORT);
+    }
+
+    public void OnClientConnect(NetworkMessage netMsg)
+    {
+        connectionsReady.Add(netMsg.conn.connectionId, false);
+
+        AppartmentLoadingMessage appartmentLoadingMessage = new AppartmentLoadingMessage
+        {
+            appartmentScale = currentAppartment.transform.localScale,
+            appartmentPosition = currentAppartment.transform.position,
+            modelName = currentAppartment.transform.name
+        };
+
+        NetworkServer.SendToClient(netMsg.conn.connectionId, VirtualHausMessageTypes.APPARTMENT_LOADING, appartmentLoadingMessage);
+    }
+    public void OnClientAppartmentLoaded(NetworkMessage netMsg)
+    {
+        List<SavedGameObject> savedGameObjects = new List<SavedGameObject>();
+        foreach (GameObject editableGameObject in GameObject.FindGameObjectsWithTag("Furniture"))
+        {
+            savedGameObjects.Add(new SavedGameObject(editableGameObject.name, editableGameObject.transform));
+        }
+
+        FurnitureLoadingMessage furnitureLoadingMessage = new FurnitureLoadingMessage
+        {
+            jsonFurnitures = JsonUtility.ToJson(savedGameObjects)
+        };
+
+        NetworkServer.SendToClient(netMsg.conn.connectionId, VirtualHausMessageTypes.FURNITURE_LOADING, furnitureLoadingMessage);
+    }
+    public void OnClientReady(NetworkMessage netMsg)
+    {
+        connectionsReady[netMsg.conn.connectionId] = true;
+    }
+}
